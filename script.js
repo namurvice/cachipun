2debc14607bed512961540d105a3532980ada19d
Vicente <vnamur@colegioelbelloto.com>
2024-07-15 15:34:10 -0400

    function de la jugada del usuario para que cambien las imagenes

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ function MostrarNombre(){
 }
 
 /*const azar = Math.florr(math.random() * 3) + 1;
-const juUsuario = document.getElementById('jusuario)*/
+const juUsuario = document.getElementById('jusuario) esto se puede guardar en una variable*/
 
-function turnoJugador(){
-   const azar = math.florr(math.random() * 3) + 1;
-   const juUsuario = document.getElementById(jusuario); 
+function jugadaUsuario(jugada){
+   if (jugada==1){
+      document.querySelector('#Jusuario').src="img/piedra.png";
+   }else if(jugada==2){
+      document.querySelector('#Jusuario').src="img/papel.png";
+   }else if(jugada==3){
+      document.querySelector('#Jusuario').src="img/tijera.png";
+   }
 }
