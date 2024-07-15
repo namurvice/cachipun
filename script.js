4244e451ba1386edda4fbad05a3aa5fb24227737
Vicente <vnamur@colegioelbelloto.com>
2024-07-15 14:55:34 -0400

    este es el primer commit primer guardado de todo

---
+++ script.js
@@ -0,0 +1,7 @@
+function MostrarNombre(){
+   let Nombre = document.querySelector('#nombreUsuario').value;
+   document.querySelector('#jugador1Nombre').textContent = Nombre;
+}
+
+/*const azar = Math.florr(math.random() * 3) + 1;
+const juUsuario = document.getElementById('jusuario)*/
